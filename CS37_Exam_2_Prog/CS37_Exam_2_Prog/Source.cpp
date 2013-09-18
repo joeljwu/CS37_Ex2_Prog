5ca42aa4c9a761355a945a732305bdb4a8221414
rgjr <rdgutierrez@gmail.com>
2013-09-18 13:01:57 -0700

    Setting up templates

--- CS37_Exam_2_Prog/CS37_Exam_2_Prog/Source.cpp
+++ CS37_Exam_2_Prog/CS37_Exam_2_Prog/Source.cpp
@@ -9,7 +9,7 @@ using namespace std;
 /*
 	Modify arrtemp.cpp by removing the preassignment of the data for each array and by adding five new function templates:
 		(WARNING: Make sure to include the line template <class T> before each function template)
-		1. allow the user to enter the array data from the keyboard
+		x allow the user to enter the array data from the keyboard
 		2. print the smallest and second smallest values for an array *without sorting*
 			You may assume that each array contains distinct data, that is no duplicates
 		3. sort the array data in descending order
@@ -23,17 +23,8 @@ using namespace std;
 	You will also need to show all three text files which you can do from any text editor when submitting this program
 */
 
-//function template to print an array
+//function template to load an array
 //works for multiple data types
-
-template <class T>
-void printarray (T *a, const int n)
-{
-	for (int i = 0; i < n; i++)
-		cout << a[i] << "  ";
-	cout << endl;
-}
-
 template <class T>
 void loadArray( T *a, const int n)
 {
@@ -42,6 +33,43 @@ void loadArray( T *a, const int n)
         cin >> a[i];
     }
     cin.ignore(); //will need this for strings
+}
+
+//template to print an array
+template <class T>
+void printarray (T *a, const int n)
+{
+	for (int i = 0; i < n; i++)
+		cout << a[i] << "  ";
+	cout << endl;
+}
+
+//template to find smallest number without sorting
+template <class T>
+void findSmalls(T *a, const int n)
+{
+	//
+}
+
+//template to sort array
+template <class T>
+void sort(T *a, const int n)
+{
+	//
+}
+
+//template to save to file
+template <class T>
+void saveFile(T *a, const int n)
+{
+	//
+}
+
+//template to load from file
+template <class T>
+void loadFile(T *a, const int n)
+{
+	//
 }
 
 int main()
@@ -59,6 +87,31 @@ int main()
 
 	cout << "Enter a string" << endl;
 	loadArray(c,n3);
+
+	//Similar calls should be used to print out data
+    //findSmalls(a, n1);
+    //findSmalls(b, n2);
+    //findSmalls(c, n3);
+
+    //sort(a, n1);
+    //sort(b, n2);
+    //sort(c, n3);
+
+    //printArray(a, n1);
+    //printArray(b, n2);
+    //printArray(c, n2);
+
+    //save(a, n1); //This is not right as-is, only a generalization
+    //save(b, n2);
+    //save(c, n3);
+
+    //retrieve(a, n1);
+    //retrieve(b, n2);
+    //retrieve(c, n3);
+
+    //printArray(a, n1);
+    //printArray(b, n2);
+    //printArray(c, n3);
 
 	return 0;
 }
